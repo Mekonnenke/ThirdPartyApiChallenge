b468dcc7029d181ecf5e8fb5a6ff24bcd76e5950
Mona Lisa <mekotigi@gmail.com>
2021-11-21 00:32:59 -0500

    Commit jquey for date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+<input type="text" value ="today();" id="datepicker"/>
+JS:
+
+ $('#datepicker').datepicker();
+function today(){
+    var d = new Date();
+    var curr_date = d.getDate();
+    var curr_month = d.getMonth() + 1;
+    var curr_year = d.getFullYear();
+    document.write(curr_date + "-" + curr_month + "-" + curr_year);
+}
 
 
 $(function()
@@ -14,3 +25,19 @@ function displaytime(){
     setTimeout('displaytime()', 1000);
 }
 
+
+// modal is fully visible
+$("#task-form-modal").on("shown.bs.modal", function() {
+  // highlight textarea
+  $("#modalTaskDescription").trigger("focus");
+});
+
+
+$(document).ready(function(){
+    $(".table-2").mouseover(function(){
+      $(".table-2").css("background-color", "green");
+    });
+    $(".table-2").mouseout(function(){
+      $(".table-2").css("background-color", "lightgray");
+    });
+  });
